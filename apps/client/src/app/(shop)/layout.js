5488bce80dfc20daa272fc50634b8be9be91a3f7
saqibam92 <saqibam0@gmail.com>
2025-12-23 19:19:23 +0600

    layout updated

--- apps/client/src/app/(shop)/layout.js
+++ apps/client/src/app/(shop)/layout.js
@@ -9,7 +9,7 @@ import MobileBottomNav from "@/components/layout/MobileBottomNav";
 
 export default function ShopLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen pb-20 md:pb-0">
       <Header />
       <Breadcrumbs />
       <main className="flex-grow">{children}</main>
